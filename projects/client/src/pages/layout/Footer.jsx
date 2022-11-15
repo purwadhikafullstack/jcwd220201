301d928936d3ab935ec62bc26468f3617c76989c
RGS <nuradlifathan@gmail.com>
2022-11-15 23:42:58 +0700

    footer skeleton #1

---
+++ projects/client/src/pages/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/react"
+
+const Footer = () => {
+  return (
+    <>
+      <Box>
+        <Heading>FOOTER</Heading>
+      </Box>
+    </>
+  )
+}
+export default Footer
