1dbd53813382dab804c5d53901dd9b2bc64713ad
fraya <fitrah.fraya@gmail.com>
2022-11-21 21:55:52 +0700

    fixed #2

--- projects/server/controllers/authController.js
+++ projects/server/controllers/authController.js
@@ -51,26 +51,6 @@ const authController = {
       });
     }
   },
-  refreshToken: async (req, res) => {
-    try {
-      const findUserById = await User.findByPk(req.user.id);
-
-      const renewedToken = signToken({
-        id: req.user.id,
-      });
-
-      return res.status(200).json({
-        message: "Renewed user token",
-        data: findUserById,
-        token: renewedToken,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Server error",
-      });
-    }
-  },
 };
 
 module.exports = authController;
--- projects/server/routes/authRoute.js
+++ projects/server/routes/authRoute.js
@@ -4,6 +4,5 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/login", authController.loginUser);
-router.get("/refresh_token", verifyToken, authController.refreshToken);
 
 module.exports = router;
